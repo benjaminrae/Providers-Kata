d839aaae4c4f11ce4e74e32f41e9ab52b8feb7f3
Benjamin Rae <benjamin.rae@qualifyze.com>
2025-01-16 20:23:03 +0100

    test: set up tasks

--- src/EventService.test.ts
+++ src/EventService.test.ts
@@ -1,5 +1,5 @@
-import { EventService } from "./EventService";
-import { InMemoryEventRepository } from "./InMemoryEventRepository";
+import { EventService } from './EventService'
+import { InMemoryEventRepository } from './InMemoryEventRepository'
 
 describe('EventService', () => {
   let eventService: EventService
@@ -10,6 +10,9 @@ describe('EventService', () => {
     eventService = new EventService(eventRepository)
   })
 
+  /*
+   * TASK 1: Refactor the event service so that we can remove the use of expect.any(...)
+   * */
   it('should schedule an event', () => {
     eventService.scheduleEvent('Event Name', 60)
 
@@ -21,10 +24,14 @@ describe('EventService', () => {
     })
   })
 
+  /*
+   * TASK 2: Refactor so that we can remove the use of expect.any(...)
+   * and decouple the test from the implementation details
+   * */
   it('should schedule an event (slow)', async () => {
     const startTime = new Date()
     const endTime = new Date(startTime.getTime() + 60 * 60000)
-    await new Promise(resolve => setTimeout(resolve, 1000))
+    await new Promise(resolve => setTimeout(resolve, 1))
 
     eventService.scheduleEvent('Event Name', 60)
 
@@ -36,16 +43,21 @@ describe('EventService', () => {
     })
   })
 
+  /*
+   * TASK 3: This test was passing yesterday, but today it is not passing,
+   * refactor to make sure it always passes
+   * */
   it('should schedule an event with a custom start date', () => {
-    const startDate = new Date(2021, 0, 1, 12, 0);
+    const startTime = new Date(2025, 0, 16, 20, 0)
+    const endTime = new Date(2025, 0, 16, 21, 0)
 
-    eventService.scheduleEvent('Event Name', 60, startDate)
+    eventService.scheduleEvent('Event Name', 60, startTime)
 
     expect(eventService.checkPendingEvents()).toContainEqual({
       id: expect.any(String),
       name: 'Event Name',
-      startTime: startDate,
-      endTime: new Date(2021, 0, 1, 13, 0),
+      startTime: startTime,
+      endTime: endTime,
     })
   })
 
